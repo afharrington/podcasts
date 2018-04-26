053d82350c649387dc2f847523fa8356404c1c36
annafharrington <annafkh@gmail.com>
2018-04-26 18:29:35 +0900

    Added links to browse page

--- src/components/PodcastGridItem/index.js
+++ src/components/PodcastGridItem/index.js
@@ -7,17 +7,21 @@ class PodcastGridItem extends Component {
 
   render () {
     const podcast = this.props.podcast;
+    console.log(podcast);
 
     return (
-      <div className='podcast-grid-item'>
-        <img src={podcast.imageUrl} alt='' />
-        <p className='podcast-grid-item-title'>{podcast.title}</p>
-        {/*
-        <p className='podcast-grid-item-episode'>{podcast.podcastEpisodeTitle}</p>
-        <p className='podcast-grid-item-date'>
-          <Moment fromNow date={podcast.podcastEpisodeDate}/>
-        </p> */}
-      </div>
+        <div className='podcast-grid-item'>
+          <a href={`/podcast/${podcast.id}`}>
+            <img src={podcast.imageUrl} alt='' />
+          </a>
+          <p className='podcast-grid-item-title'>{podcast.title}</p>
+          {/*
+          <p className='podcast-grid-item-episode'>{podcast.podcastEpisodeTitle}</p>
+          <p className='podcast-grid-item-date'>
+            <Moment fromNow date={podcast.podcastEpisodeDate}/>
+          </p> */}
+        </div>
+
     )
   }
 }
